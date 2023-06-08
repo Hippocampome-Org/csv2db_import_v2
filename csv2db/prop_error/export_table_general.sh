223be15c209892068d26be77b5f824bd35d7f64b
Nate Sutton <natemsutton@gmail.com>
2023-06-08 00:53:06 -0400

    added new inter-buton distance variables to NPS and NOC results

--- csv2db/prop_error/export_table_general.sh
+++ csv2db/prop_error/export_table_general.sh
@@ -1,21 +1,21 @@
 table=$1;
 command="sed -n 6p ../../database_save.sh" # username
 command2=$(eval $command)
-command3="echo $command2 | cut -c19-10000"
+command3="echo $command2 | cut -c12-10000"
 USER=$(eval $command3)
 command="sed -n 7p ../../database_save.sh" # password
 command2=$(eval $command)
-command3="echo $command2 | cut -c23-10000"
+command3="echo $command2 | cut -c16-10000"
 PASS=$(eval $command3)
 command="sed -n 8p ../../database_save.sh" # database
 command2=$(eval $command)
-command3="echo $command2 | cut -c22-10000"
+command3="echo $command2 | cut -c15-10000"
 DB=$(eval $command3)
 ADDR=localhost # db address
 CSV_DIR=csv # import csv files location
 EXP_DIR="/var/tmp/SynproExports/" # export csv files directory
 
-command="rm /var/tmp/SynproExports/$table";
+command="rm /var/tmp/SynproExports/$table.csv";
 eval $command;
 
 echo "SET STATEMENT max_statement_time=0 FOR SELECT * FROM \
