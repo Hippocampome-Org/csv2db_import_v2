6645d1606e6431ed60f679b1726623d1e46240d3
Nate Sutton <natemsutton@gmail.com>
2023-06-15 21:51:24 -0400

    added code to exclude perisomatic connections and any other unwanted connections from being reported with synaptic probabilities values

--- csv2db/prop_error/order_of_pairs.sql
+++ csv2db/prop_error/order_of_pairs.sql
@@ -1,3 +1,7 @@
+# WHERE statement with potential_synapses, number_of_contacts, and probability
+# is for filtering out excluded connections, e.g., perisomatic connections that
+# don't target a dendrite, which is represented as a neuron pair to exclude by
+# the entries for these columns being absent in the SynproNOCR data
 CREATE VIEW SynproPairsOrder AS
 SELECT DISTINCT
     nc.source_ID,
@@ -6,3 +10,7 @@ SELECT DISTINCT
     SUBSTRING_INDEX(SUBSTRING_INDEX(nc.layers, ':', 2),':', - 1) AS parcel
 FROM
     SynproNOCR as nc
+WHERE
+    potential_synapses != ''
+AND number_of_contacts != ''
+AND probability != ''
