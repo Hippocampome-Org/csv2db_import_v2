1bf790d8a6ca972ce9027d42d80fac2056cb9097
Kasturi Nadella <knadella@gmu.edu>
2025-02-02 11:13:25 -0500

    Commti as of Feb 1 2025, this file is located at /root/GA4analytics/run_analytics_import_load.sh

--- GA_analytics/run_analytics_import_load_server.sh
+++ GA_analytics/run_analytics_import_load_server.sh
@@ -1,26 +1,24 @@
-#!/bin/bash
+#!/bin/bash 
 
-# This script runs a Python script to import Google Analytics data into a database.
-# It requires elevated privileges.
+PYTHON_EXEC="/usr/bin/python3.7"  # Set the correct Python version
+PYTHON_SCRIPT="/var/www/html/csv2db/GA_analytics/import_load.py"
+LOG_FILE="/var/log/GA4analytics/error.log"
 
-# Define the path to the Python script
-PYTHON_SCRIPT="/var/www/html/csv2db_import_v2/GA_analytics/import_load.py"
-
-# Check if the script has executable permissions
 if [ ! -x "$PYTHON_SCRIPT" ]; then
-    echo "Error: The script is not executable. Please check the file permissions."
+    echo "ERROR: The script is not executable. Please check the file permissions."
     exit 1
 fi
 
-# Execute the Python script with elevated privileges
-echo "Starting the import process..."
-sudo /usr/bin/python3.7 "$PYTHON_SCRIPT"
+echo "Starting the import process ..." 
+output=$($PYTHON_EXEC "$PYTHON_SCRIPT" 2>&1)
+exit_status=$?
 
-# Check the exit status of the last command
-if [ $? -eq 0 ]; then
+echo "$output"
+
+if [ $exit_status -eq 0 ]; then
     echo "Import process completed successfully."
 else
     echo "Import process failed. Check the logs for errors."
+    echo "$output" >> "$LOG_FILE"
     exit 1
 fi
-~                                                                               
